2f231f7e5a03317d042bf1181090db3e4970cbfe
Polaris_Light <49135577+GLDYM@users.noreply.github.com>
2026-04-21 10:54:44 +0800

    Fix formatting in test-client-connect.sh script

--- .github/scripts/test-client-connect.sh
+++ .github/scripts/test-client-connect.sh
@@ -173,6 +173,7 @@ portablemc \
   --main-dir "$CLIENT_DIR/.minecraft" \
   start "$selected_target" \
   -u "$USERNAME" \
+  --jvm "-Xmx8G -Xms8G" \
   --join-server "$SERVER_HOST" \
   --join-server-port "$SERVER_PORT" > "$CLIENT_DIR/client.log" 2>&1 &
 
